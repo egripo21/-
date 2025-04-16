16c563be4902e7d0679b72ecf49446a875662f7f
egripo21 <melix7017@gmail.com>
2025-04-16 14:53:39 +0300

    Add files via upload

---
+++ функции/max.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace def
+{
+    internal class max
+    {
+        static int fac(int[] array)
+        {
+            int max = 0;
+            foreach (int num in array) {
+                if (num > max) {
+                    max = num;
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine(maximum(array));
+
+
+        }
+    }
+}
